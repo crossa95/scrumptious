1ab79f3da59a1f221405fea1a825956ba27ca203
Caitlin Foley <47759649+RoseNight3@users.noreply.github.com>
2020-03-07 16:17:34 -0500

    Add files via upload

---
+++ Card System/src/views/Project.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="project">
+        <h1>Project</h1>
+        <CardSystem msg="Card Board"/>
+    </div>
+</template>
+<script>
+import CardSystem from '@/components/CardSystem.vue'
+export default {
+  name: 'Project',
+  components: {
+    CardSystem
+  }
+}
+</script>
+<style scoped>
+    h1{
+        margin-top: 0 auto;
+    }
+</style>
